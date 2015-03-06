74b6544c2f4e2c91babe9dd82d5a757fc89b92c4
dharmi4u <dharmi.net@gmail.com>
2015-03-06 23:52:12 +0530

    comitting files

---
+++ OOPS/OOPS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticInheritanceB obj = new StaticInheritanceB();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOPS/OOPS/StaticInheritance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPS
+{
+     class StaticInheritanceA 
+    {
+        public StaticInheritanceA()
+        {
+            Console.WriteLine("Constructor A");
+        }
+         static StaticInheritanceA()
+        {
+            Console.WriteLine("Static Constructor A");
+        }
+    }
+     class  StaticInheritanceB : StaticInheritanceA
+    {
+        public StaticInheritanceB()
+        {
+            Console.WriteLine("Constructor B");
+        }
+        static StaticInheritanceB()
+        {
+            Console.WriteLine("Static Constructor B");
+        }
+    }
+}
